b1247b7bb29cd83f60a1dc1c216b88457bbf090b
Jeffrey Cooper <jcooper37@cnm.edu>
2017-04-10 11:06:48 -0600

    Add files

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>A Lot About Jeffrey Cooper</title>
+	</head>
+	<body>
+		<img src="images/jc01.jpg" >
+		<p><h1>Jeffrey Cooper</h1></p>
+		<p><h3>Web Developer</h3></p>
+		<p><h3>Musician</h3></p>
+	</body>
+</html>
