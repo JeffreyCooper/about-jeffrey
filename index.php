4ae188f8b07c73477538956c0ac5956556102d60
Gerrit Van Dyke <gvandyke1@cnm.edu>
2017-04-10 13:29:50 -0600

    Added star wars preference

--- index.php
+++ index.php
@@ -9,5 +9,6 @@
 		<p><h3>Web Developer</h3></p>
 		<p><h3>Musician</h3></p>
 		<p>I think Star Wars is way cooler than Star Trek!</p>
+		<p>Sometimes when I get bored I sing showtunes to myself and it gets me back in the moment.</p>
 	</body>
 </html>
